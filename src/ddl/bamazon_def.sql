d8649795f4081f13504f60f8a241976dacd3ed29
SBC-VN <vnestohr@juno.com>
2019-05-02 19:11:16 -0400

    Initial database definition.

---
+++ src/ddl/bamazon_def.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  product_name VARCHAR(60) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  department_name VARCHAR(40),
+  stock_quantity INT NOT NULL
+);
+
+ALTER TABLE `bamazon`.`products` 
+ADD UNIQUE INDEX `product_name_UNIQUE` (`product_name` ASC) VISIBLE;
+
+
+CREATE TABLE transactions (
+  transaction_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  date DATE NOT NULL,
+  item_id INT NOT NULL,
+  quantity INT NULL,
+  description VARCHAR(80)
+);
+
+ALTER TABLE `bamazon`.`transactions` 
+ADD INDEX `validate_product_idx` (`item_id` ASC) VISIBLE;
+
+ALTER TABLE `bamazon`.`transactions` 
+ADD CONSTRAINT `validate_product`
+  FOREIGN KEY (`item_id`)
+  REFERENCES `bamazon`.`products` (`item_id`)
+  ON DELETE RESTRICT
+  ON UPDATE RESTRICT;
