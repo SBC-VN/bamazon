982a0b8c981e5736072692493955d456a6f25b7b
SBC-VN <vnestohr@juno.com>
2019-05-03 20:10:03 -0400

    Working section 1.

--- src/bamazonCustomer.js
+++ src/bamazonCustomer.js
@@ -1,6 +1,8 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+var productList = [];
+
 var connection = mysql.createConnection({
     host: "localhost",
   
@@ -13,47 +15,98 @@ var connection = mysql.createConnection({
     // Your password
     password: "none",
     database: "bamazon"
-  });
+});
 
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  displayProducts();
+  displayProducts();  
 });
 
+// Not a full blown constructor, but a placeholder for one if needed.
+function extractProductInfo(datarow) {
+  var ProductInfo = {
+    item_id : datarow.item_id,
+    product_name : datarow.product_name,
+    price : datarow.price,
+    department_name : datarow.department_name,
+    stock_quantity : datarow.stock_quantity};
+  return ProductInfo;
+} 
+
+// Read the database, extract the products into an array - then call the next function
+//  down the line (select the product & quantity).
 function displayProducts() {
   connection.query("SELECT item_id, product_name, price, department_name, stock_quantity FROM products", 
     function(err, res) {
       if (err) throw err;
+      else {
+        for (var i = 0; i < res.length; i++) {
+          productList.push(extractProductInfo(res[i]));
+        }
+        selectProduct();
+      }
+    });
+  }
 
-      inquirer
-        .prompt([
-          {
-            name: "choice",
-            type: "rawlist",
-            choices: function() {
-              var choiceArray = [];
-              for (var i = 0; i < res.length; i++) {
-                choiceArray.push(res[i].product_name);
-              }
-              return choiceArray;
-            },
-            message: "What item do you want to buy?"
-          },
-          {
-            name: "quantity",
-            type: "input",
-            message: "How many would you like?"
-          }
-        ])
-        .then(function(answer) {
-          // get the information of the chosen item
-          var chosenItem;
-          for (var i = 0; i < results.length; i++) {
-            if (results[i].item_name === answer.choice) {
-              chosenItem = results[i];
+function selectProduct() {
+  inquirer
+    .prompt([
+      {
+        name: "choice",
+        type: "rawlist",
+        choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < productList.length; i++) {
+              choiceArray.push({name: productList[i].product_name,value: productList[i].item_id});
             }
-          }
-        });
-      });
-    }
+            return choiceArray;
+          },
+        message: "What item do you want to buy?"
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like?"
+      }
+      ])
+    .then(function(answer) {
+      console.log(" ");
+      var chosenItem = productList[answer.choice-1];
+      var chosenQty = parseInt(answer.quantity);
+      if (chosenQty > chosenItem.stock_quantity) {
+        console.log("No can buy!  There are only " + chosenItem.stock_quantity + " in stock.")
+      }
+      else {
+        recordPurchase(chosenItem,chosenQty);
+      }
+    });
+}
+
+function recordPurchase(item, quantity) {
+  console.log("Purchasing " + item.product_name);
+  console.log("  Quantity " + quantity);
+  console.log("Unit Price " + item.price);
+  console.log("Total Cost " + quantity * item.price);
+
+  var newQty = item.stock_quantity - quantity;
+
+  var query = connection.query(
+    "UPDATE products SET ? WHERE ?",
+    [
+      {
+        stock_quantity: newQty
+      },
+      {
+        item_id: item.item_id
+      }
+    ],
+    function(err, res) {
+      if (err) {
+        console.log("Error",err);
+      }
+      else {
+        console.log("Confirmed");
+      }
+    });
+}    
